969824d8e56db69fd7a1d34f59acb0412e5b06b7
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2022-07-29 12:56:26 +0200

    Removed deprecated files
    During the development a few functions got deprecated and I remove them now.

--- lib/alchemy-pg_search.rb
+++ lib/alchemy-pg_search.rb
@@ -1,18 +1,12 @@
 require "alchemy/pg_search/engine"
 require "alchemy/pg_search/config"
 require "alchemy/pg_search/search"
-require "alchemy/pg_search/page_result"
-require "alchemy/pg_search/page_search_scope"
 
 module Alchemy
   module PgSearch
     SEARCHABLE_ESSENCES = %w[EssenceText EssenceRichtext EssencePicture]
-    DEFAULT_CONFIG = {
-      page_search_scope: PageSearchScope.new,
-    }
 
     extend Config
-    self.config = DEFAULT_CONFIG
 
     def self.is_searchable_essence?(essence_type)
       SEARCHABLE_ESSENCES.include?(essence_type)
--- lib/alchemy/pg_search/search.rb
+++ lib/alchemy/pg_search/search.rb
@@ -48,37 +48,6 @@ module Alchemy
 
         query
       end
-
-      ##
-      # search for pages with the given query
-      #
-      # @param query [string] the search string
-      # @return [array<PageResult>] found page results
-      # @deprecated
-      def self.search_pages(query)
-        results = {}
-
-        raw_search(query).each do |document|
-          page = document.searchable_type == "Alchemy::Page" ? document.searchable : document.searchable.page
-
-          next unless page
-
-          results[page.id] = PageResult.new(page) unless results.has_key? page.id
-          results[page.id].add_relative_document document
-        end
-
-        results.values
-      end
-
-      private
-
-      ##
-      # @param query [string]
-      # @return [array<::PgSearch::Document>]
-      # @deprecated
-      def self.raw_search(query)
-        ::PgSearch.multisearch(query).includes(searchable: { all_elements: { contents: :essence } })
-      end
     end
   end
 end
