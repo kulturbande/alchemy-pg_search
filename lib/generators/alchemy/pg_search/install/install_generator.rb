791f5cf5193a5e0795d660a81f3231587da49a19
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2022-08-25 22:32:02 +0200

    Add install generator
    - removed the old pg_search_document migration
    - added the pg_search multiselect - generator and the page migration to generator

---
+++ lib/generators/alchemy/pg_search/install/install_generator.rb
@@ -0,0 +1,25 @@
+require "rails/generators"
+require "rails/generators/active_record/migration"
+
+module Alchemy
+  module PgSearch
+    class InstallGenerator < ::Rails::Generators::Base
+      include ActiveRecord::Generators::Migration
+
+      desc "Install Alchemy PgSearch - Gem into Rails App."
+
+      source_root(File.expand_path("../../../..", __dir__))
+
+      def install_migrations
+        # Install pg_search multisearch - migration
+        generate("pg_search:migration:multisearch", abort_on_failure: true)
+
+        # copy page migration
+        migration_template File.expand_path("./templates/add_page_id_column_to_pg_search_documents.tt", __dir__), "db/migrate/add_page_id_column_to_pg_search_documents.rb"
+
+        # run migrations
+        rake("db:migrate", abort_on_failure: true)
+      end
+    end
+  end
+end
