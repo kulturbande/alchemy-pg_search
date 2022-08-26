8dcda402ac003b3970ac684b8d70dda32ea31caa
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2022-08-26 15:06:46 +0200

    Moved the page migration to db/migration - folder
    It is easier to read and is lesser code

--- lib/generators/alchemy/pg_search/install/install_generator.rb
+++ lib/generators/alchemy/pg_search/install/install_generator.rb
@@ -1,6 +1,5 @@
 require "rails/generators"
 require "rails/generators/active_record/migration"
-require "rails/generators/migration"
 
 module Alchemy
   module PgSearch
@@ -12,18 +11,12 @@ module Alchemy
       source_root(File.expand_path("../../../..", __dir__))
 
       def install_migrations
-        # Copy the migrations of the gem
-        rake("alchemy_pg_search:install:migrations", abort_on_failure: true)
-
         # Install pg_search multisearch - migration - the pg_search is not testing if the migration already exists
         generate("pg_search:migration:multisearch", abort_on_failure: true) unless self.class.migration_exists?("db/migrate", 'create_pg_search_documents')
 
-        # copy page migration
-        migration_template(
-          File.expand_path("./templates/add_page_id_column_to_pg_search_documents.tt", __dir__),
-          "db/migrate/add_page_id_column_to_pg_search_documents.rb"
-        )
-
+        # Copy the migrations of the gem
+        rake("alchemy_pg_search:install:migrations", abort_on_failure: true)
+        
         # run migrations
         rake("db:migrate", abort_on_failure: true)
       end
