f85ad28feb2210de6ac5379da818c8efa583b67a
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2022-08-26 14:57:08 +0200

    Add own migrations to install script

--- lib/generators/alchemy/pg_search/install/install_generator.rb
+++ lib/generators/alchemy/pg_search/install/install_generator.rb
@@ -1,5 +1,6 @@
 require "rails/generators"
 require "rails/generators/active_record/migration"
+require "rails/generators/migration"
 
 module Alchemy
   module PgSearch
@@ -11,11 +12,17 @@ module Alchemy
       source_root(File.expand_path("../../../..", __dir__))
 
       def install_migrations
-        # Install pg_search multisearch - migration
-        generate("pg_search:migration:multisearch", abort_on_failure: true)
+        # Copy the migrations of the gem
+        rake("alchemy_pg_search:install:migrations", abort_on_failure: true)
+
+        # Install pg_search multisearch - migration - the pg_search is not testing if the migration already exists
+        generate("pg_search:migration:multisearch", abort_on_failure: true) unless self.class.migration_exists?("db/migrate", 'create_pg_search_documents')
 
         # copy page migration
-        migration_template File.expand_path("./templates/add_page_id_column_to_pg_search_documents.tt", __dir__), "db/migrate/add_page_id_column_to_pg_search_documents.rb"
+        migration_template(
+          File.expand_path("./templates/add_page_id_column_to_pg_search_documents.tt", __dir__),
+          "db/migrate/add_page_id_column_to_pg_search_documents.rb"
+        )
 
         # run migrations
         rake("db:migrate", abort_on_failure: true)
